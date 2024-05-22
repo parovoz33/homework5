16b3a621eb07b10fc14a1f743297a18bb3c8070e
parovoz33 <parovoz33@MAIL.RU>
2024-05-22 15:03:19 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+str_='Boldininy'
+int_=12345
+bool_= False
+immutable_var=str_,int_,bool_
+print(immutable_var)
+immutable_var[1]=54321
+print(immutable_var)
+mutable_list=[str_,int_,bool_]
+mutable_list[1]=54321
+print(mutable_list)
